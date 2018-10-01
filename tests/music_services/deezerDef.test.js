6d34d8b63ac698dad5b9d356aa718883f49fe77e
Anthony Reinette <dantho361@hotmail.com>
2018-10-01 16:59:40 +0200

    adds basic unit testing for query parsing for the Deezer Service

--- tests/music_services/deezerDef.test.js
+++ tests/music_services/deezerDef.test.js
@@ -1,5 +1,35 @@
-function sum(a,b) { return a + b }
+const deezerDef = require('../../lib/music_services/deezerDef');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('getSearchTerm should parse artist parameter correctly', () => {
+  var type = "artist"
+  var term = "artist:toots"
+  var artist = ""
+  var album = ""
+  var track = ""
+
+  expect(deezerDef.term(type, term, artist, album, track))
+  .toBe(encodeURIComponent('artist:toots'))
 });
+
+test('getSearchTerm should parse artist parameter correctly', () => {
+  var type = "artist"
+  var term = "artist:La Rue Kétanou"
+  var artist = ""
+  var album = ""
+  var track = ""
+
+  expect(deezerDef.term(type, term, artist, album, track))
+    .toBe(encodeURIComponent('artist:La Rue Kétanou'))
+});
+
+test('getSearchTerm should parse artist & track parameters correctly', () => {
+  var type = "artist"
+  var term = "artist:La Rue Kétanou:track:Ma faute à toi"
+  var artist = ""
+  var album = ""
+  var track = ""
+
+  expect(deezerDef.term(type, term, artist, album, track))
+    .toBe(encodeURIComponent('artist:La Rue Kétanou track:Ma faute à toi'))
+});
+
