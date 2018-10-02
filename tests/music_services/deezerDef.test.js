a6bb659462a74a57a8a86e50f26b20066a0121cc
Anthony Reinette <dantho361@hotmail.com>
2018-10-02 13:45:28 +0200

    adds tests for Deezer definitions

--- tests/music_services/deezerDef.test.js
+++ tests/music_services/deezerDef.test.js
@@ -2,34 +2,34 @@ const deezerDef = require('../../lib/music_services/deezerDef');
 
 test('getSearchTerm should parse artist parameter correctly', () => {
   var type = "artist"
-  var term = "artist:toots"
+  var term = 'artist:"toots"'
   var artist = ""
   var album = ""
   var track = ""
 
   expect(deezerDef.term(type, term, artist, album, track))
-  .toBe(encodeURIComponent('artist:toots'))
+  .toBe(encodeURIComponent('artist:"toots"'))
 });
 
 test('getSearchTerm should parse artist parameter correctly', () => {
   var type = "artist"
-  var term = "artist:La Rue Kétanou"
+  var term = 'artist:"La Rue Kétanou"'
   var artist = ""
   var album = ""
   var track = ""
 
   expect(deezerDef.term(type, term, artist, album, track))
-    .toBe(encodeURIComponent('artist:La Rue Kétanou'))
+    .toBe(encodeURIComponent('artist:"La Rue Kétanou"'))
 });
 
 test('getSearchTerm should parse artist & track parameters correctly', () => {
   var type = "artist"
-  var term = "artist:La Rue Kétanou:track:Ma faute à toi"
+  var term = 'artist:"La Rue Kétanou":track:"Ma faute à toi"'
   var artist = ""
   var album = ""
   var track = ""
 
   expect(deezerDef.term(type, term, artist, album, track))
-    .toBe(encodeURIComponent('artist:La Rue Kétanou track:Ma faute à toi'))
+    .toBe(encodeURIComponent('artist:"La Rue Kétanou" track:"Ma faute à toi"'))
 });
 
