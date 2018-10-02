973a5a43d2d144967d2f86829e191e2021877ce7
Anthony Reinette <dantho361@hotmail.com>
2018-10-02 13:46:16 +0200

    adds unit tests for music search terms

--- tests/actions/musicSearch.test.js
+++ tests/actions/musicSearch.test.js
@@ -1,5 +1,57 @@
-function sum(a, b) { return a + b }
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+const musicSearch = require('../../lib/actions/musicSearch');
+
+test('extractEntitiesFromTerm parses correctly artist', () => {
+    var term = 'artist:"Toots and the Maytals"'
+    var mockedApi = {
+      registerAction: jest.fn()
+    }
+
+    var [artist, album, track] = musicSearch(mockedApi).extractEntities(term)
+    
+    expect(artist).toBe("Toots and the Maytals")
+    expect(album).toBeUndefined()
+    expect(track).toBeUndefined()
+
+  });
+
+test('extractEntitiesFromTerm parses correctly album', () => {
+    var term = 'album:"Astroworld"'
+    var mockedApi = {
+      registerAction: jest.fn()
+    }
+
+    var [artist, album, track] = musicSearch(mockedApi).extractEntities(term)
+    
+    expect(artist).toBeUndefined()
+    expect(album).toBe("Astroworld")
+    expect(track).toBeUndefined()
+
+  });
+    
+  test('extractEntitiesFromTerm parses correctly track', () => {
+    var term = 'track:"Stargazing"'
+    var mockedApi = {
+      registerAction: jest.fn()
+    }
+
+    var [artist, album, track] = musicSearch(mockedApi).extractEntities(term)
+    
+    expect(artist).toBeUndefined()
+    expect(album).toBeUndefined()
+    expect(track).toBe("Stargazing")
+
   });
   
+  test('extractEntitiesFromTerm parses correctly track + artist', () => {
+    var term = 'artist:"Travis $cott":track:"Stargazing"'
+    var mockedApi = {
+      registerAction: jest.fn()
+    }
+
+    var [artist, album, track] = musicSearch(mockedApi).extractEntities(term)
+    
+    expect(artist).toBe("Travis $cott")
+    expect(album).toBeUndefined()
+    expect(track).toBe("Stargazing")
+
+  });
